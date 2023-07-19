0a63f5d0e3db447128f177987a339711c1f41f42
”weigaohui“ <1991504760qq.com>
2023-07-19 14:59:22 +0800

    "吉顺办中台"

---
+++ src/components/common/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer f-c-c flex-col text-14 color="#6a6a6a">
+    <p>
+      Copyright © 2023-present
+    </p>
+  </footer>
+</template>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+/** 重置样式 */
+import '@/styles/reset.css'
+import 'uno.css'
+import '@/styles/global.scss'
+import 'virtual:svg-icons-register'
+
+import { createApp } from 'vue'
+import { setupRouter } from '@/router'
+import { setupStore } from '@/store'
+import App from './App.vue'
+import { useResize } from '@zclzone/utils'
+import { setupNaiveDiscreteApi } from './utils'
+
+async function setupApp() {
+  const app = createApp(App)
+
+  setupStore(app)
+  setupNaiveDiscreteApi()
+
+  await setupRouter(app)
+
+  app.use(useResize)
+  app.mount('#app')
+}
+
+setupApp()
